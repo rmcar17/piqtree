16ed603b806be9ca27b407098f55113eb4a5f1d6
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2025-03-17 13:26:38 +1100

    TST: updated tests to using cogent3 new_type

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,20 +11,20 @@ def DATA_DIR() -> pathlib.Path:
 
 @pytest.fixture
 def three_otu(DATA_DIR: pathlib.Path) -> ArrayAlignment:
-    aln = load_aligned_seqs(DATA_DIR / "example.fasta", moltype="dna")
+    aln = load_aligned_seqs(DATA_DIR / "example.fasta", moltype="dna", new_type=True)
     aln = aln.take_seqs(["Human", "Rhesus", "Mouse"])
     return aln.omit_gap_pos(allowed_gap_frac=0)
 
 
 @pytest.fixture
 def four_otu(DATA_DIR: pathlib.Path) -> ArrayAlignment:
-    aln = load_aligned_seqs(DATA_DIR / "example.fasta", moltype="dna")
+    aln = load_aligned_seqs(DATA_DIR / "example.fasta", moltype="dna", new_type=True)
     aln = aln.take_seqs(["Human", "Chimpanzee", "Rhesus", "Mouse"])
     return aln.omit_gap_pos(allowed_gap_frac=0)
 
 
 @pytest.fixture
 def five_otu(DATA_DIR: pathlib.Path) -> ArrayAlignment:
-    aln = load_aligned_seqs(DATA_DIR / "example.fasta", moltype="dna")
+    aln = load_aligned_seqs(DATA_DIR / "example.fasta", moltype="dna", new_type=True)
     aln = aln.take_seqs(["Human", "Chimpanzee", "Rhesus", "Manatee", "Dugong"])
     return aln.omit_gap_pos(allowed_gap_frac=0)
