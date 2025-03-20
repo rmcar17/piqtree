a8e8476349aa4705586101a77f3d4da941f2fce4
Robert McArthur <robert.mcarthur@anu.edu.au>
2025-03-20 19:16:15 +1100

    DEV: normalise slashes in before_all_windows

--- build_tools/before_all_windows.sh
+++ build_tools/before_all_windows.sh
@@ -1,8 +1,10 @@
 # Install dependencies using choco
 
-echo "Boost_INCLUDE_DIR: " $Boost_INCLUDE_DIR
-echo "Boost_LIBRARY_DIRS: " $Boost_LIBRARY_DIRS
-echo "BOOST_ROOT: " $BOOST_ROOT
+export Boost_INCLUDE_DIR=$(echo $Boost_INCLUDE_DIR | sed 's|\\|/|g')
+export Boost_LIBRARY_DIRS=$(echo $Boost_LIBRARY_DIRS | sed 's|\\|/|g')
+
+echo "Boost_INCLUDE_DIR: $Boost_INCLUDE_DIR"
+echo "Boost_LIBRARY_DIRS: $Boost_LIBRARY_DIRS"
 
 choco install -y llvm --version=14.0.6 --allow-downgrade
 choco install -y eigen 
