d10a3e3f8b11276e1e7eb7f6646b260e6cd2e24c
Robert McArthur <robert.mcarthur@anu.edu.au>
2025-06-24 16:49:43 +1000

    TST: Use mock dataset for testing download dataset

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,7 @@
 import pathlib
 
+from pytest_mock import MockerFixture
+
 import piqtree
 
 
@@ -8,6 +10,24 @@ def test_dataset_names() -> None:
     assert len(names) > 0
 
 
-def test_download_dataset(tmp_path: pathlib.Path) -> None:
-    path = piqtree.download_dataset("example.tree.gz", dest_dir=tmp_path)
-    assert pathlib.Path(path).exists()
+def test_download_dataset(tmp_path: pathlib.Path, mocker: MockerFixture) -> None:
+    # Mock the requests response
+    fake_content = b"fake dataset data"
+
+    mock_response = mocker.Mock()
+    mock_response.iter_content.return_value = [fake_content]
+    mocker.patch("piqtree._data.requests.get", return_value=mock_response)
+
+    dataset_name = "example.tree.gz"
+    dest_path = piqtree.download_dataset(dataset_name, dest_dir=tmp_path)
+
+    assert dest_path.exists()
+    assert dest_path.read_bytes() == fake_content
+
+    # Check correct arguments
+    expected_url = piqtree._data._get_url(dataset_name)
+    piqtree._data.requests.get.assert_called_once_with(
+        expected_url,
+        stream=True,
+        timeout=20,
+    )
