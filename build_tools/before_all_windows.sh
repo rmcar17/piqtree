1f611db0afdc678a9d2e5ea9f0621a980945f571
Robert McArthur <robert.mcarthur@anu.edu.au>
2025-06-30 15:44:11 +1000

    DEV: Fix IQ-TREE building on CI (#268)
    * DEV: attempt to use correct llvm
    
    * DEV: attempt properly resolving path
    
    * DEV: more debugging of the folders
    
    * DEV: attempt to properly glob
    
    * DEV: debug install locations
    
    * DEV: correct install dir for llvm
    
    * DEV: use cygpath
    
    * DEV: remove debug echos
    
    * DEV: remove another debug echo

--- build_tools/before_all_windows.sh
+++ build_tools/before_all_windows.sh
@@ -9,5 +9,11 @@ echo "Boost_LIBRARY_DIRS: $Boost_LIBRARY_DIRS"
 choco install -y llvm --version=14.0.6 --allow-downgrade
 choco install -y eigen 
 
+LLVM_BIN=$(cygpath -u "${PROGRAMFILES}\\LLVM\\bin")
+export PATH="$LLVM_BIN:$PATH"
+
+export CC=clang.exe
+export CXX=clang++.exe
+
 # Build IQ-TREE
 bash build_tools/build_iqtree.sh
