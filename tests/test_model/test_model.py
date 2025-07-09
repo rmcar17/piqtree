99413bb0f8b22dbb204b75b0e35b258cd0107577
Robert McArthur <robert.mcarthur@anu.edu.au>
2025-07-09 15:12:38 +1000

    TST: Extend tests for make model with parameterisation - also reduce quantity

--- tests/test_model/test_model.py
+++ tests/test_model/test_model.py
@@ -4,6 +4,7 @@ import pytest
 
 from piqtree.model import (
     AaModel,
+    CustomBaseFreq,
     DiscreteGammaModel,
     FreeRateModel,
     FreqType,
@@ -16,45 +17,35 @@ from piqtree.model import (
 )
 
 
-@pytest.mark.parametrize(
-    "sub_mod",
-    [
-        *StandardDnaModel.iter_available_models(),
-        *LieModel.iter_available_models(),
-        *AaModel.iter_available_models(),
-    ],
-)
-@pytest.mark.parametrize("freq_type", [None, *list(FreqType)])
-@pytest.mark.parametrize("invariable_sites", [False, True])
-@pytest.mark.parametrize(
-    "rate_model",
-    [
-        None,
-        DiscreteGammaModel(),
-        FreeRateModel(),
-        DiscreteGammaModel(6),
-        FreeRateModel(6),
-    ],
-)
-def test_make_model(
+def check_make_model(
     sub_mod: SubstitutionModel,
-    freq_type: FreqType,
-    invariable_sites: bool,
-    rate_model: RateModel,
+    freq_type: FreqType | CustomBaseFreq | None = None,
+    rate_model: RateModel | None = None,
+    *,
+    invariable_sites: bool | float = False,
 ) -> None:
     model = Model(sub_mod, freq_type, rate_model, invariable_sites=invariable_sites)
     expected = str(model)
 
     # Check the expected string is approximately generated correctly
-    assert invariable_sites == model.invariable_sites
-    if invariable_sites:
-        assert "+I" in expected
+
+    # float and True should be True, False should be False
+    assert (invariable_sites is not False) == model.invariable_sites
+
+    if not isinstance(invariable_sites, bool):
+        assert re.search(r"\+I\{\d+(\.\d+)?\}", expected)
+        assert model.proportion_invariable_sites == invariable_sites
+        assert model.invariable_sites
     else:
-        assert "+I" not in expected
+        assert model.proportion_invariable_sites is None
+        assert model.invariable_sites == invariable_sites
+        assert ("+I" in expected) == invariable_sites
 
     if isinstance(rate_model, DiscreteGammaModel):
         assert isinstance(model.rate_model, DiscreteGammaModel)
         assert "+G" in expected
+        if rate_model.alpha is not None:
+            assert re.search(r"\+G\d*\{\d+(\.\d+)?\}", expected)
     else:
         assert not isinstance(model.rate_model, DiscreteGammaModel)
         assert "+G" not in expected
@@ -62,18 +53,130 @@ def test_make_model(
     if isinstance(rate_model, FreeRateModel):
         assert isinstance(model.rate_model, FreeRateModel)
         assert "+R" in expected
+        if rate_model.rates is not None:
+            assert re.search(r"\+R\d*\{\d+(\.\d+)?(,\d+(\.\d+)?)*\}", expected)
     else:
         assert not isinstance(model.rate_model, FreeRateModel)
         assert "+R" not in expected
 
     if freq_type is not None:
         assert "+F" in expected
+        if isinstance(freq_type, CustomBaseFreq):
+            assert re.search(r"\+F\{\d+(\.\d+)?(,\d+(\.\d+)?)*\}", expected)
     else:
         assert "+F" not in expected
 
     # Check make_model
     got = str(make_model(expected))
     assert got == expected
+
+
+@pytest.mark.parametrize(
+    "sub_mod",
+    [
+        *StandardDnaModel.iter_available_models(),
+        StandardDnaModel.GTR([4.39, 5.30, 4.39, 1.0, 12.1]),
+        StandardDnaModel.TIM2([4.39, 5.30, 12.1]),
+        *LieModel.iter_available_models(),
+        *AaModel.iter_available_models(),
+    ],
+)
+def test_make_model_sub_mod(sub_mod: SubstitutionModel) -> None:
+    check_make_model(sub_mod)
+
+
+@pytest.mark.parametrize(
+    "sub_mod",
+    [
+        StandardDnaModel.iter_available_models()[0],
+        LieModel.iter_available_models()[0],
+        AaModel.iter_available_models()[0],
+    ],
+)
+@pytest.mark.parametrize(
+    "freq_type",
+    [*list(FreqType), CustomBaseFreq([0.1, 0.2, 0.3, 0.4])],
+)
+def test_make_model_freq_type(
+    sub_mod: SubstitutionModel,
+    freq_type: FreqType | CustomBaseFreq,
+) -> None:
+    check_make_model(sub_mod, freq_type=freq_type)
+
+
+@pytest.mark.parametrize(
+    "sub_mod",
+    [
+        StandardDnaModel.iter_available_models()[-1],
+        LieModel.iter_available_models()[-1],
+        AaModel.iter_available_models()[-1],
+    ],
+)
+@pytest.mark.parametrize("invariable_sites", [0.2, True])
+def test_make_model_invar_sites(
+    sub_mod: SubstitutionModel,
+    invariable_sites: bool | float,
+) -> None:
+    check_make_model(sub_mod, invariable_sites=invariable_sites)
+
+
+@pytest.mark.parametrize(
+    "sub_mod",
+    [
+        StandardDnaModel.iter_available_models()[1],
+        LieModel.iter_available_models()[1],
+        AaModel.iter_available_models()[1],
+    ],
+)
+@pytest.mark.parametrize(
+    "rate_model",
+    [
+        DiscreteGammaModel(),
+        DiscreteGammaModel(6),
+        DiscreteGammaModel(2, 0.3),
+        FreeRateModel(),
+        FreeRateModel(6),
+        FreeRateModel(2, [0.4, 0.6], [0.9, 0.1]),
+    ],
+)
+def test_make_model_rate_model(
+    sub_mod: SubstitutionModel,
+    rate_model: RateModel,
+) -> None:
+    check_make_model(sub_mod, rate_model=rate_model)
+
+
+@pytest.mark.parametrize(
+    "sub_mod",
+    [
+        *StandardDnaModel.iter_available_models()[:3],
+        StandardDnaModel.GTR([1.0, 2.0, 1.5, 3.7, 2.8]),
+        *LieModel.iter_available_models()[:3],
+        *AaModel.iter_available_models()[:3],
+    ],
+)
+@pytest.mark.parametrize(
+    "freq_type",
+    [None, FreqType.FQ, CustomBaseFreq([0.1, 0.2, 0.3, 0.4])],
+)
+@pytest.mark.parametrize("invariable_sites", [False, 0.2, True])
+@pytest.mark.parametrize(
+    "rate_model",
+    [
+        None,
+        DiscreteGammaModel(6),
+        DiscreteGammaModel(2, 0.3),
+        FreeRateModel(),
+        FreeRateModel(2, [0.4, 0.6], [0.9, 0.1]),
+    ],
+)
+def test_make_model(
+    sub_mod: SubstitutionModel,
+    freq_type: FreqType | CustomBaseFreq | None,
+    invariable_sites: bool | float,
+    rate_model: RateModel | None,
+) -> None:
+    check_make_model(sub_mod, freq_type, rate_model, invariable_sites=invariable_sites)
 
 
 def test_model_repr() -> None:
