e8b83ff7e0b2701a55676f27ad0be2d4798fd727
Robert McArthur <robert.mcarthur@anu.edu.au>
2025-07-10 11:55:34 +1000

    TST: Test parameterised protein model

--- tests/test_iqtree/test_protein_build_tree.py
+++ tests/test_iqtree/test_protein_build_tree.py
@@ -2,11 +2,24 @@ import pytest
 from cogent3.core.alignment import Alignment
 
 import piqtree
+from piqtree import make_model
 from piqtree.model import (
     AaModel,
     Model,
     SubstitutionModel,
 )
+
+
+def check_build_tree_model(
+    aln: Alignment,
+    model: Model,
+    *,
+    coerce_str: bool = False,
+) -> None:
+    got = piqtree.build_tree(aln, str(model) if coerce_str else model, rand_seed=1)
+
+    # Check if all branch lengths exist
+    assert all("length" in v.params for v in got.get_edge_vector())
 
 
 def check_build_tree(
@@ -16,11 +29,7 @@ def check_build_tree(
     coerce_str: bool = False,
 ) -> None:
     model = Model(aa_model)
-
-    got = piqtree.build_tree(aln, str(model) if coerce_str else model, rand_seed=1)
-
-    # Check if all branch lengths exist
-    assert all("length" in v.params for v in got.get_edge_vector())
+    check_build_tree_model(aln, model, coerce_str=coerce_str)
 
 
 @pytest.mark.parametrize("aa_model", AaModel.iter_available_models())
@@ -31,3 +40,10 @@ def test_protein_build_tree(protein_four_otu: Alignment, aa_model: AaModel) -> N
 @pytest.mark.parametrize("aa_model", AaModel.iter_available_models())
 def test_str_protein_build_tree(protein_four_otu: Alignment, aa_model: AaModel) -> None:
     check_build_tree(protein_four_otu, aa_model, coerce_str=False)
+
+
+def test_parameterised_model(protein_four_otu: Alignment) -> None:
+    base_freq_values = map(str, [0.05] * 20)
+
+    model = make_model(f"rtREV+F{{{','.join(base_freq_values)}}}+I{{0.2}}+G3{{1.2}}")
+    check_build_tree_model(protein_four_otu, model)
