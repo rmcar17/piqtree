fad156f1868a7ae986f365a6cf37dea14f1fd962
Robert McArthur <robert.mcarthur@anu.edu.au>
2025-11-13 14:29:47 +1000

    MAINT: Add more paths for eigen3

--- build_tools/before_all_mac.sh
+++ build_tools/before_all_mac.sh
@@ -9,4 +9,7 @@ export LDFLAGS="-L$(brew --prefix libomp)/lib"
 export CPPFLAGS="-I$(brew --prefix libomp)/include -I$(brew --prefix eigen@3)/include"
 export CXXFLAGS="-I$(brew --prefix libomp)/include"
 
+export CMAKE_PREFIX_PATH="$(brew --prefix eigen@3):${CMAKE_PREFIX_PATH:-}"
+export PKG_CONFIG_PATH="$(brew --prefix eigen@3)/share/pkgconfig:${PKG_CONFIG_PATH:-}"
+
 bash build_tools/build_iqtree.sh
