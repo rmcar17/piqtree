7ff48b7e3575ab9c97be64f62ab2415a8c5a5612
Robert McArthur <robert.mcarthur@anu.edu.au>
2025-11-13 14:05:34 +1000

    MAINT: Include eigen include path

--- build_tools/before_all_mac.sh
+++ build_tools/before_all_mac.sh
@@ -6,7 +6,7 @@ fi
 brew install llvm eigen@3 boost libomp make
 
 export LDFLAGS="-L$(brew --prefix libomp)/lib"
-export CPPFLAGS="-I$(brew --prefix libomp)/include"
+export CPPFLAGS="-I$(brew --prefix libomp)/include -I$(brew --prefix eigen@3)/include"
 export CXXFLAGS="-I$(brew --prefix libomp)/include"
 
 bash build_tools/build_iqtree.sh
